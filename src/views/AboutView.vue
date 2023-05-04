b00ce021475fdc81174d78ea9e5f1457dbc83ccd
wsl <3562398700@qq.com>
2023-05-04 15:09:06 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>修改成功了嘛</h1>
+    <h1>This is an about page</h1>
+    <h1>大多数的</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      info: '华庆涛1111111111119900000',
+      msg: 'dsjkfdjkdfkj'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
